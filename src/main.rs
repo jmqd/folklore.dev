6b74fa38d3b8ad4e1152a2903a7192d03f1fffc1
Jordan McQueen <j@jm.dev>
2020-10-21 21:46:44 -0700

    Support exact text match AND N unigram queries
    This lets you search for something like this:
    
    ```
    "steve jobs" swift books
    ```
    
    And it will only return documents containing the exact phrase "steve job" AND
    all of the words `["swift", "books"]`.
    
    See example output here:
    
    ```
    "steve jobs" favorite books swift
    34 bytes read
    "steve jobs" favorite books swift
    
    Parsed query: Query {
        exact_ngram: Some(
            [
                "steve",
                "jobs",
            ],
        ),
        unigrams: Some(
            [
                "favorite",
                "books",
                "swift",
            ],
        ),
    }
    Query results: Some(
        {
            "https://oleb.net/blog",
        },
    )
    ```

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,13 @@ impl<'i> Index<'i> {
             None => None,
         }
     }
+
+    fn exact_ngram_match(&self, ngram: &Vec<String>) -> Option<HashSet<String>> {
+        match ngram.len() {
+            1 => self.unigram_match(&ngram[0]),
+            _ => self.ngram_match(&ngram),
+        }
+    }
 }
 
 #[tokio::main]
@@ -193,14 +200,58 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
             ),
         },
     };
-
     println!("Parsed query: {:#?}", query);
-    match query.exact_ngram {
+
+    let mut unigram_result_set = HashSet::new();
+    if query.unigrams.is_some() {
+        let unigrams = query.unigrams.clone().unwrap();
+        let mut iter = unigrams.iter();
+
+        // We seed the result set with the first unigram result set.
+        match index.unigram_match(&iter.next().unwrap()) {
+            None => return None,
+            Some(results) => results.into_iter().for_each(|p| {
+                unigram_result_set.insert(p);
+            }),
+        }
+
+        // All other unigram result sets will iteratively perform set intersection
+        // with the result set, to generate the final set of result candidates
+        for unigram in iter {
+            match index.unigram_match(&unigram) {
+                Some(results) => {
+                    unigram_result_set = unigram_result_set
+                        .intersection(&results)
+                        .map(|s| s.to_string())
+                        .collect();
+                }
+                None => (),
+            }
+        }
+    }
+
+    let exact_results = match query.exact_ngram.clone() {
         None => None,
-        Some(ngram) => match ngram.len() {
-            1 => index.unigram_match(&ngram[0]),
-            _ => index.ngram_match(&ngram),
-        },
+        Some(ngram) => index.exact_ngram_match(&ngram),
+    };
+
+    match (query.exact_ngram.is_some(), query.unigrams.is_some()) {
+        // The query is only asking for an exact string search.
+        (true, false) => exact_results,
+
+        // The query only wants to match N unigrams.
+        (false, true) => Some(unigram_result_set),
+
+        // The query is meaningless.
+        (false, false) => None,
+
+        // The query wants the intersection of an exact ngram search and N unigrams AND'd.
+        (true, true) => Some(
+            unigram_result_set
+                .intersection(&exact_results.unwrap())
+                .map(|s| s.to_string())
+                .collect(),
+        ),
     }
 }
 
