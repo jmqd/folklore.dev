2e6c45db33002cb249b39e3aa561ba801a639516
Jordan McQueen <j@jm.dev>
2020-10-22 17:57:18 -0700

    Add crawling, document_codes, BiMaps

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #[macro_use]
 extern crate lazy_static;
 
+use bimap::BiMap;
 use bincode;
 use itertools::Itertools;
 use regex::Regex;
 use reqwest;
 use select::document::Document;
 use select::predicate::Any;
+use select::predicate::Name;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
 use std::iter::Iterator;
+use url::{ParseError, Url};
 
 #[derive(Debug)]
 struct Query {
@@ -39,41 +42,132 @@ struct Website {
 /// unigrams: A mapping from all words to all documents those words appear in.
 ///
 /// ngrams: A mapping from all ngrams to all documents those ngrams appear in.
-#[derive(Serialize, Deserialize)]
-struct Index<'i> {
-    #[serde(borrow)]
-    pub unigrams: HashMap<String, HashSet<&'i str>>,
+struct Index {
+    //#[serde(borrow)]
+    pub unigrams: HashMap<usize, HashSet<usize>>,
 
-    #[serde(borrow)]
-    pub ngrams: HashMap<Vec<String>, HashSet<&'i str>>,
+    //#[serde(borrow)]
+    pub ngrams: HashMap<Vec<usize>, HashSet<usize>>,
 
-    pub indexed_sites: HashSet<String>
+    pub document_codes: BiMap<String, usize>,
+
+    pub word_codes: BiMap<String, usize>,
 }
 
-impl<'i> Index<'i> {
-    fn unigram_match(&self, unigram: &str) -> Option<HashSet<String>> {
-        Index::pass_page_results(self.unigrams.get(unigram))
+impl Index {
+    fn get_or_generate_word_code(&mut self, word: String) -> usize {
+        match self.word_codes.get_by_left(&word) {
+            Some(code) => *code,
+            None => {
+                self.word_codes.insert(word, self.word_codes.len());
+                self.word_codes.len() - 1
+            }
+        }
     }
 
-    fn ngram_match(&self, ngram: &[String]) -> Option<HashSet<String>> {
-        Index::pass_page_results(self.ngrams.get(ngram))
+    fn get_or_generate_document_code(&mut self, document_id: String) -> usize {
+        match self.document_codes.get_by_left(&document_id) {
+            Some(code) => *code,
+            None => {
+                self.document_codes
+                    .insert(document_id, self.document_codes.len());
+                self.document_codes.len() - 1
+            }
+        }
     }
 
-    fn pass_page_results(page_results: Option<&HashSet<&str>>) -> Option<HashSet<String>> {
+    fn index_texts(&mut self, document_id: String, texts: HashSet<Vec<String>>) {
+        let document_code = self.get_or_generate_document_code(document_id);
+
+        for ngram in texts.into_iter() {
+            for unigram in ngram.clone().into_iter() {
+                self.insert_unigram(unigram, document_code);
+            }
+
+            for bigram in ngram.clone().into_iter().tuple_windows::<(_, _)>() {
+                let bigram_vec = vec![bigram.0, bigram.1];
+                self.insert_ngram(bigram_vec, document_code);
+            }
+        }
+
+        println!("indexed sites length: {:#?}", self.document_codes.len());
+        println!("unigram index length: {:#?}", self.unigrams.len());
+        println!("ngram index length: {:#?}", self.ngrams.len());
+        println!("word_codes length: {:#?}", self.word_codes.len());
+    }
+
+    fn insert_unigram(&mut self, unigram: String, document_code: usize) {
+        let code = self.get_or_generate_word_code(unigram);
+
+        if self.unigrams.contains_key(&code) {
+            self.unigrams.get_mut(&code).unwrap().insert(document_code);
+        } else {
+            let mut set = HashSet::with_capacity(1);
+            set.insert(document_code);
+            self.unigrams.insert(code, set);
+        }
+    }
+
+    fn insert_ngram(&mut self, ngram: Vec<String>, document_code: usize) {
+        let ngram_codes = ngram
+            .into_iter()
+            .map(|w| self.get_or_generate_word_code(w))
+            .collect::<Vec<usize>>();
+
+        if self.ngrams.contains_key(&ngram_codes) {
+            self.ngrams
+                .get_mut(&ngram_codes)
+                .unwrap()
+                .insert(document_code);
+        } else {
+            let mut set = HashSet::with_capacity(1);
+            set.insert(document_code);
+            self.ngrams.insert(ngram_codes, set);
+        }
+    }
+
+    fn unigram_match(&self, unigram: String) -> Option<HashSet<String>> {
+        match self.word_codes.get_by_left(&unigram) {
+            Some(code) => self.pass_page_results(self.unigrams.get(code)),
+            None => None,
+        }
+    }
+
+    fn ngram_match(&self, ngram: Vec<String>) -> Option<HashSet<String>> {
+        let ngram_codes = ngram
+            .into_iter()
+            .map(|w| self.word_codes.get_by_left(&w))
+            .collect::<Vec<Option<&usize>>>();
+
+        if ngram_codes.iter().any(|c| c.is_none()) {
+            return None;
+        }
+
+        let ngram_codes: Vec<usize> = ngram_codes.into_iter().map(|c| *c.unwrap()).collect();
+
+        self.pass_page_results(self.ngrams.get(&ngram_codes))
+    }
+
+    fn pass_page_results(&self, page_results: Option<&HashSet<usize>>) -> Option<HashSet<String>> {
         match page_results {
             // If we found some pages that matches the search query:
             // We copy all the page URLs into a return value for the caller.
-            Some(page_results) => Some(page_results.into_iter().map(|p| p.to_string()).collect()),
+            Some(page_results) => Some(
+                page_results
+                    .into_iter()
+                    .map(|p| self.document_codes.get_by_right(p).unwrap().to_string())
+                    .collect(),
+            ),
 
             // Otherwise, their search query had no results.
             None => None,
         }
     }
 
-    fn exact_ngram_match(&self, ngram: &Vec<String>) -> Option<HashSet<String>> {
+    fn exact_ngram_match(&self, ngram: Vec<String>) -> Option<HashSet<String>> {
         match ngram.len() {
-            1 => self.unigram_match(&ngram[0]),
-            _ => self.ngram_match(&ngram),
+            1 => self.unigram_match(ngram[0].clone()),
+            _ => self.ngram_match(ngram),
         }
     }
 }
@@ -96,24 +190,33 @@ async fn run<'i>(config: &mut Config) {
         }
     }
 
-    let mut index = Index {
-        unigrams: HashMap::new(),
-        ngrams: HashMap::new(),
-        indexed_sites: HashSet::new()
-    };
+    let index = build_index(&config.websites).await;
 
-    for website in &config.websites {
-        println!("Fetching {:#?}...", &website.url);
-        match fetch(&website.url).await {
-            Some(document) => index_texts(extract_texts(&document), &mut index, &website.url),
-            None => (),
-        };
-    }
-    let serialized = bincode::serialize(&index).expect("Could not encode bincode.");
-    std::fs::write("/tmp/index.bin", serialized).expect("Could not write to file!");
     loop {
         cli_testing(&index);
     }
+}
+
+async fn build_index<'i>(websites: &'i Vec<Website>) -> Index {
+    let mut index = Index {
+        unigrams: HashMap::new(),
+        ngrams: HashMap::new(),
+        document_codes: BiMap::new(),
+        word_codes: BiMap::new(),
+    };
+
+    for website in websites {
+        // println!("Fetching {:#?}...", &website.url);
+        for (document, id) in crawl(&website.url).await {
+            match document {
+                Some(document) => {
+                    index.index_texts(id, extract_texts(&document));
+                }
+                None => (),
+            };
+        }
+    }
+    index
 }
 
 fn cli_testing(index: &Index) {
@@ -128,13 +231,72 @@ fn cli_testing(index: &Index) {
     }
 }
 
+async fn crawl(url: &str) -> Vec<(Option<Document>, String)> {
+    let mut documents = Vec::new();
+    let mut urls = Vec::new();
+    let root = Url::parse(url).unwrap();
+    documents.push((fetch(url).await, url.to_string()));
+    documents[0].0.as_ref().map(|d| {
+        d.find(Name("a")).for_each(|node| {
+            let link = match node.attr("href") {
+                None => None,
+                Some(link) => Some(Url::parse(link)),
+            };
+
+            let link = match link {
+                Some(Ok(link)) => {
+                    if link.host() == root.host() {
+                        // println!("Links are on same host: {:#?} and {:#?}", root, link);
+                        Some(link)
+                    } else {
+                        None
+                    }
+                }
+                Some(Err(e)) => match e {
+                    ParseError::RelativeUrlWithoutBase => {
+                        match root.clone().join(node.attr("href").unwrap()) {
+                            Ok(link) => Some(link),
+                            Err(e) => {
+                                println!("Error when trying to fix link: {:#?}", e);
+                                None
+                            }
+                        }
+                    }
+                    _ => {
+                        println!("Error with link: {:#?}", e);
+                        None
+                    }
+                },
+                _ => None,
+            };
+
+            if link.is_some() {
+                urls.push(link.unwrap());
+            }
+        });
+    });
+
+    for link in urls {
+        documents.push((fetch(&link.to_string()).await, link.to_string()));
+    }
+
+    //println!("{:#?} more links to get: {:#?}", urls.len(), urls);
+    documents
+}
+
 async fn fetch(url: &str) -> Option<Document> {
     let cache_path: std::path::PathBuf = std::path::Path::new(&format!(
         "/tmp/{}",
         url.trim_start_matches("https://")
             .trim_start_matches("http://")
+            .trim_end_matches("/")
     ))
     .to_path_buf();
+
+    if cache_path.to_str().unwrap().to_string().contains("..") {
+        println!("Nasty hackers... {:#?}", cache_path);
+        return None;
+    }
 
     if std::fs::metadata(std::path::Path::new(&cache_path)).is_ok() {
         return Some(Document::from(
@@ -178,6 +340,7 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
     lazy_static! {
         static ref QUERY_PARSER: Regex = Regex::new("(?:\"(.*)\")?\\s(.+)?").unwrap();
     }
+
     let captures = QUERY_PARSER.captures(&query_str).unwrap();
     let query = Query {
         exact_ngram: match captures.get(1) {
@@ -208,10 +371,10 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
     let mut unigram_result_set = HashSet::new();
     if query.unigrams.is_some() {
         let unigrams = query.unigrams.clone().unwrap();
-        let mut iter = unigrams.iter();
+        let mut iter = unigrams.into_iter();
 
         // We seed the result set with the first unigram result set.
-        match index.unigram_match(&iter.next().unwrap()) {
+        match index.unigram_match(iter.next().unwrap()) {
             None => return None,
             Some(results) => results.into_iter().for_each(|p| {
                 unigram_result_set.insert(p);
@@ -221,7 +384,7 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
         // All other unigram result sets will iteratively perform set intersection
         // with the result set, to generate the final set of result candidates
         for unigram in iter {
-            match index.unigram_match(&unigram) {
+            match index.unigram_match(unigram) {
                 Some(results) => {
                     unigram_result_set = unigram_result_set
                         .intersection(&results)
@@ -235,7 +398,7 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
 
     let exact_results = match query.exact_ngram.clone() {
         None => None,
-        Some(ngram) => index.exact_ngram_match(&ngram),
+        Some(ngram) => index.exact_ngram_match(ngram),
     };
 
     match (query.exact_ngram.is_some(), query.unigrams.is_some()) {
@@ -256,48 +419,6 @@ fn query(query_str: String, index: &Index) -> Option<HashSet<String>> {
                 .collect(),
         ),
     }
-}
-
-fn index_texts<'i>(texts: HashSet<Vec<String>>, index: &mut Index<'i>, url: &'i str) {
-    for ngram in texts.into_iter() {
-        for unigram in ngram.clone().into_iter() {
-            if index.unigrams.contains_key(&unigram) {
-                index.unigrams.get_mut(&unigram).unwrap().insert(url);
-            } else {
-                let mut set = HashSet::with_capacity(1);
-                set.insert(url);
-                index.unigrams.insert(unigram, set);
-            }
-        }
-
-        for bigram in ngram.clone().into_iter().tuple_windows::<(_, _)>() {
-            let bigram_vec = vec![bigram.0, bigram.1];
-            if index.ngrams.contains_key(&bigram_vec) {
-                index.ngrams.get_mut(&bigram_vec).unwrap().insert(url);
-            } else {
-                let mut set = HashSet::with_capacity(1);
-                set.insert(url);
-                index.ngrams.insert(bigram_vec, set);
-            }
-        }
-
-        for trigram in ngram.into_iter().tuple_windows::<(_, _, _)>() {
-            let trigram_vec = vec![trigram.0, trigram.1, trigram.2];
-            if index.ngrams.contains_key(&trigram_vec) {
-                index.ngrams.get_mut(&trigram_vec).unwrap().insert(url);
-            } else {
-                let mut set = HashSet::with_capacity(1);
-                set.insert(url);
-                index.ngrams.insert(trigram_vec, set);
-            }
-        }
-    }
-
-    index.indexed_sites.insert(url.to_string());
-
-    println!("indexed sites length: {:#?}", index.indexed_sites.len());
-    println!("unigram index length: {:#?}", index.unigrams.len());
-    println!("ngram index length: {:#?}", index.ngrams.len());
 }
 
 fn has_search_terms(s: &str) -> bool {
